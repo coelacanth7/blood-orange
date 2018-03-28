e102c9855192804fe9993cfc91a343023e5195f0
Coelacanth7 <galford151@gmail.com>
2018-03-28 01:00:54 -0700

    user api updating mongo collections

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -6,6 +6,13 @@ const Hashes = require("jshashes");
 var MD5 = new Hashes.MD5();
 const router = express.Router();
 
+const mongoose = require("mongoose");
+var MongooseModels = require("./../mongoose/models");
+var User = mongoose.model("User");
+
+const wikiUrl =
+	"https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=1&format=json&origin=*&rnnamespace=0";
+
 router.post("/user", async (req, res) => {
 	try {
 		const ip = requestIp.getClientIp(req);
@@ -19,23 +26,23 @@ router.post("/user", async (req, res) => {
 			response.body.latitude === "" || !response.body.latitude ? false : true;
 		fprint.locationData = response.body;
 
-		const hash = MD5.hex(JSON.stringify(fprint));
+		const fingerprinthash = MD5.hex(JSON.stringify(fprint));
 
-		res.json({ hash: hash, location: response.body });
+		let user = await User.findOne({ fingerprinthash });
+		if (!user) {
+			const response = await got(wikiUrl);
+			const username = JSON.parse(response.body).query.random[0].title;
+			user = await new User({ fingerprinthash, username });
+			await user.save((err, user) => {
+				if (err) console.log(err);
+				console.log(user);
+			});
+		}
+
+		res.json({ fingerprinthash, location: response.body, user });
 	} catch (error) {
 		console.log(error);
 	}
-	// request(`https://freegeoip.net/json/${ip}`, function(error, response, body) {
-	// 	console.log("error:", error);
-	// 	console.log("statusCode:", response && response.statusCode);
-	//
-	// 	fprint.hasLocation = body.latitude === "" || !body.latitude ? false : true;
-	// 	fprint.locationData = body;
-	//
-	// 	const hash = MD5.hex(JSON.stringify(fprint));
-	//
-	// 	res.json({ hash: hash, location: body });
-	// });
 });
 
 module.exports = router;

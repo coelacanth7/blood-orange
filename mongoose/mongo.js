77e4b0962b5889ce7249a4e38d041d777e3af09d
Coelacanth7 <galford151@gmail.com>
2018-03-28 01:54:29 -0700

    update mongo config for heroku

--- mongoose/mongo.js
+++ mongoose/mongo.js
@@ -3,8 +3,14 @@ var env = process.env.NODE_ENV || "development";
 var config = require("./config")[env];
 
 module.exports = () => {
-	var envUrl = process.env[config.use_env_variable];
+	var envUrl = process.env.MONGODB_URI || process.env[config.use_env_variable];
 	var localUrl = `mongodb://${config.host}/${config.database}`;
 	var mongoUrl = envUrl ? envUrl : localUrl;
-	return mongoose.connect(mongoUrl);
+	return mongoose.connect(mongoUrl, function(err, res) {
+		if (err) {
+			console.log("ERROR connecting to: " + mongoUrl + ". " + err);
+		} else {
+			console.log("Succeeded connected to: " + mongoUrl);
+		}
+	});
 };
